b9a7b3753888caf01f59c80a7dcf3a79ffe66360
ZhenX123 <95683902+ZhenX123@users.noreply.github.com>
2021-12-27 00:06:34 +0200

    Add files via upload

---
+++ Housesell/Housesell.BLL/ReaderBLL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;//DataTable,DataSet,DataRow
+using Housesell.DAL;
+
+namespace Housesell.BLL
+{
+    public class ReaderBLL
+    {
+        public DataTable GetReadList()
+        {
+            Dao dao = new Dao();
+            {
+
+
+            }
+        }
+
+    }
+}
---
+++ Housesell/Housesell.DAL/Dao.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Housesell.DAL
+
+{
+    public class Dao
+    {
+        SqlConnection sc = new SqlConnection();
+        public SqlConnection connect()
+        {
+            string str = @"Data Source=CAN-HEAR-ME;Database=Housedata;Integrated Security=True";//数据库连接字符串Database connection string
+            SqlConnection sc = new SqlConnection(str);//创建数据库连接对象Create a database connection object
+            sc.Open();//打开数据库Open the database
+            return sc;
+        }
+        public SqlCommand command(string sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql, connect());
+            return cmd;
+
+        }
+        public int Execute(string sql)//更新操作update operation
+        {
+            return command(sql).ExecuteNonQuery();
+        }
+        public SqlDataReader read(string sql)//读取操作Read operation
+        {
+            return command(sql).ExecuteReader();
+        }
+        public void DaoClose()
+        {
+            sc.Close();//关闭数据连接Closing data Connections
+        }
+    }
+
+}
+
